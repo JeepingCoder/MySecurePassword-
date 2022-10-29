23d5947703d8e7c49c466eaba0e0a7dad9cf89f0
Patrick Ingram <114114167+JeepingCoder@users.noreply.github.com>
2022-10-29 15:46:00 -0400

    all code except math

--- script.js
+++ script.js
@@ -1,16 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var lowerCaseChar = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z");
-var upperCaseChar = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","p","Q","R","S","T","U","V","W","X","Y","Z");
-var numericChar = ("1","2","3","4","5","6","7","8","9","0");
-var specialChar = ("!","@","#","$","%","^","&","*","(",")","_","-","+","=","'","{","}","[","]","|",";",":","?","<",">","/",".");
+var lowerCaseChar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCaseChar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "p", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numericChar = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "'", "{", "}", "[", "]", "|", ";", ":", "?", "<", ">", "/", "."];
 
+var allChar = [];
 
 // click generate psswd button (make active) presensted with series of prompts for pswd criteria 
-function generatePassword (){
+function generatePassword() {
+  var password = "";
+  var userinput = window.prompt(" How many characters would you like your password? 8-128")
+  console.log(userinput)
 
- var userinput = window.prompt(" How many characters would you like your password? 8-128")
- 
+  //if else statement
+  if (userinput < 8 || userinput > 128 ||  isNaN(userinput)) {
+    //Alert user an error
+    window.alert('Passowrd length needs to be a number between 8 and 128')
+    //reset the question, which means restarting the function
+    generatePassword()
+  } else {
+    var userLowCase = window.confirm(" Would you like to use lower case characters? ");
+    var userUpCase = window.confirm (" would you like to use upper case characters ?");
+    var userNumCase = window.confirm (" Would you like to use numbers? ");
+    var userSpecChar = window.confirm (" Would you like to use special characters? ");
+
+    console.log(userLowCase, userUpCase, userNumCase, userSpecChar)
+    if(userLowCase){
+      //push lowerCaseChar to allChar arr
+      allChar.push(...lowerCaseChar)
+    }
+    if(userUpCase){
+      //push lowerCaseuserUpCaseChar to allChar arr
+      allChar.push(...upperCaseChar)
+    }
+    if(userNumCase){
+      //push userNumCase to allChar arr
+      allChar.push(...numericChar)
+    }
+    if(userSpecChar){
+      //push userSpecChar to allChar arr
+      allChar.push(...specialChar)
+    }
+    console.log(allChar)
+  }
+    
+    //select random characters from allChar to create your random password
+    
+    
+
+    //for Lopp
+    //pick random charrater
+    //add random chracter to password var
+  
+
+  return password;
 }
 
 // I am prompted for password criteria: 8-128 charachters length, what typer of char upper, lower, numeric and/ or special characters
